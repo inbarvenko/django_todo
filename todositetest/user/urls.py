75dc02c6e12b272912923bc5ee8ef87754268eed
Ibarvenko <inga_barvenko@mail.ru>
2023-07-08 14:57:06 +0300

    no libraries

--- todositetest/user/urls.py
+++ todositetest/user/urls.py
@@ -4,19 +4,13 @@ from django.conf.urls import include
 from .views import (
     LoginAPIView, RegistrationAPIView, UserLogoutAPIView, UserAPIView
 )
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = 'user'
 urlpatterns = [
-    # path("register/", RegistrationAPIView.as_view(), name="create-user"),
-    # path("login/", LoginAPIView.as_view(), name="login-user"),
-    # path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
-    # path("logout/", UserLogoutAPIView.as_view(), name="logout-user"),
-    # path("", UserAPIView.as_view(), name="user-info"),
-    # path('api-auth/', include('rest_framework.urls'))
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.jwt')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("register/", RegistrationAPIView.as_view(), name="create-user"),
+    path("login/", LoginAPIView.as_view(), name="login-user"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("logout/", UserLogoutAPIView.as_view(), name="logout-user"),
+    path("", UserAPIView.as_view(), name="user-info"),
 ]
