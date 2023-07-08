9cc6fc365ff756d2e934a42045f97bad27260137
Ibarvenko <inga_barvenko@mail.ru>
2023-07-08 14:36:57 +0300

    djoser

--- todositetest/user/urls.py
+++ todositetest/user/urls.py
@@ -8,12 +8,6 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 app_name = 'user'
 urlpatterns = [
-    # path("register/", RegistrationAPIView.as_view(), name="create-user"),
-    # path("login/", LoginAPIView.as_view(), name="login-user"),
-    # path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
-    # path("logout/", UserLogoutAPIView.as_view(), name="logout-user"),
-    # path("", UserAPIView.as_view(), name="user-info"),
-    # path('api-auth/', include('rest_framework.urls'))
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
